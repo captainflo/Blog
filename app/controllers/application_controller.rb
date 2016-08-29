685902eef5f0e904e5a0ed85c6f0a091badd182a
Florian Lahitte <florianlahitte@MacBook-Air-de-Florian.local>
2016-08-29 16:32:24 +0200

    Add avatarimage V2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :avatar_cache])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :avatar_cache])
   end
 end
